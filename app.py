f1148b88dfa414e336d3b0fb34af3297cbb5a493
sqlreport <sqlreport@mac.com>
2023-02-07 00:00:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, Response
+import time
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/content') # render the content a url differnt from index. This will be streamed into the iframe
+def content():
+    def timer(t):
+        for i in range(t):
+            time.sleep(5) #put 60 here if you want to have seconds
+            yield str(i)
+    return Response(timer(10), mimetype='text/html') #at the moment the time value is hardcoded in the function just for simplicity
+
+@app.route('/')
+def index():
+    return render_template('test.html.jinja') # render a template at the index. The content will be embedded in this template
+
+if __name__ == '__main__':
+    app.run(use_reloader=False)
